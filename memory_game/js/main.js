463179dd7784155b9f338156a83fc941e9961807
Home <home@Homes-MacBook-Air.local>
2019-09-09 14:40:25 +0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "queen hearts";
+var cardTwo = "king hearts";
+var cardThree = "queen diamonds";
+var cardFour = "king diamonds";
+
+console.log("User flipped " + cardOne)
+console.log("User flipped " + cardThree)
